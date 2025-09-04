004f6944138c590f8b42ce22ab32bb0c30697a54
codebylalit <lalitkumarnamdev4645@gmail.com>
2025-09-04 16:24:39 +0530

    add @react-native-community/datetimepicker for date picking

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC2JCTwEYku-QYUrEHH7cOZCp43O_gtqnk",
@@ -14,3 +15,4 @@ const firebaseConfig = {
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+export const db = getFirestore(app);
